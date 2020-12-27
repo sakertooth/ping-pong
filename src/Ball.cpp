15e4286e8fb300d7845cffdc42a72bc0116c99df
sakertooth <sakertooth@gmail.com>
2020-12-27 13:39:27 -0500

    fix bugs with paddle collision and other improvements

---
+++ src/Ball.cpp
@@ -0,0 +1,60 @@
+#include "Ball.hpp"
+#include "Game.hpp"
+#include <effolkronium/random.hpp>
+
+using Random = effolkronium::random_static;
+
+namespace Pong
+{
+	Ball::Ball() : angle(45), speed(500) {}
+
+	void Ball::update(const float deltaTime, const sf::RectangleShape& leftPaddle, sf::Text& leftPaddleScore, const sf::RectangleShape& rightPaddle, sf::Text& rightPaddleScore)
+	{
+		const auto& window = Game::getInstance().getWindow();
+
+		if (getPosition().y - getRadius() < 0.0f)
+		{
+			angle = (360 - angle) % 360;
+			setPosition(getPosition().x, getRadius() + 0.1f);
+			Game::getInstance().getSoundManager().getBeepSound().play();
+		}
+		else if (getPosition().y + getRadius() > window.getSize().y)
+		{
+			angle = (360 - angle) % 360;
+			setPosition(getPosition().x, window.getSize().y - getRadius() - 0.1f);
+			Game::getInstance().getSoundManager().getBeepSound().play();
+		}
+		else if (getPosition().x - getRadius() < 0.0f)
+		{
+			angle = 180;
+			setPosition(rightPaddle.getPosition().x - rightPaddle.getSize().x / 2 - 10, rightPaddle.getPosition().y);
+			rightPaddleScore.setString(std::to_string(std::stoi(rightPaddleScore.getString().toAnsiString()) + 1));
+			Game::getInstance().getSoundManager().getPeepSound().play();
+		}
+		else if (getPosition().x + getRadius() > window.getSize().x)
+		{
+			angle = 0;
+			setPosition(leftPaddle.getPosition().x + leftPaddle.getSize().x / 2 + 10, leftPaddle.getPosition().y);
+			leftPaddleScore.setString(std::to_string(std::stoi(leftPaddleScore.getString().toAnsiString()) + 1));
+			Game::getInstance().getSoundManager().getPeepSound().play();
+		}
+
+		auto rightPaddleDistX = rightPaddle.getPosition().x - rightPaddle.getSize().x / 2 - getPosition().x;
+		if (getGlobalBounds().intersects(rightPaddle.getGlobalBounds()) && rightPaddleDistX < getRadius() && rightPaddleDistX > 0.0f)
+		{
+			angle = std::atan2f(Random::get(-0.9f, 0.9f), Random::get(-0.9f, -0.2f)) * 57.2957795f;
+			setPosition(getPosition().x - 0.1f, getPosition().y);
+			Game::getInstance().getSoundManager().getPlopSound().play();
+		}
+
+		auto leftPaddleDistX = getPosition().x - leftPaddle.getPosition().x - leftPaddle.getSize().x / 2;
+		if (getGlobalBounds().intersects(leftPaddle.getGlobalBounds()) && leftPaddleDistX < getRadius() && leftPaddleDistX > 0.0f)
+		{
+			angle = std::atan2f(Random::get(-0.9f, 0.9f), Random::get(0.2f, 0.9f)) * 57.2957795f;
+			setPosition(getPosition().x + 0.1f, getPosition().y);
+			Game::getInstance().getSoundManager().getPlopSound().play();
+		}
+
+		move(speed * std::cos(angle * 0.0174532925f) * deltaTime, -speed * std::sin(angle * 0.0174532925f) * deltaTime);
+	}
+}
--- src/States/TwoPlayerState.cpp
+++ src/States/TwoPlayerState.cpp
@@ -2,195 +2,68 @@
 #include	"States/MainMenuState.hpp"
 #include	"States/TwoPlayerState.hpp"
 
-#include	<iostream>
-#include	<effolkronium/random.hpp>
-
-using Random = effolkronium::random_static;
-
 namespace Pong::States
 {
-	TwoPlayerState::TwoPlayerState() : tgui::Gui(Game::getInstance().getWindow()),
-		leftPaddleScore(tgui::Label::create()),
-		rightPaddleScore(tgui::Label::create()),
-		paddleSpeed(500),
-		ballSpeed(500),
-		ballAngle(315),
-		ballIsColliding(false)
+	TwoPlayerState::TwoPlayerState() : paddleSpeed(500), ballSpeed(500), ballAngle(315)
 	{
-		const auto& window = Game::getInstance().getWindow();
-		const auto paddleSize = sf::Vector2f(1, 64);
-		const auto yCenter = (window.getSize().y - paddleSize.y) / 2;
+		const auto& window		= Game::getInstance().getWindow();
+		const auto paddleSize	= sf::Vector2f(3, 64);
+
+		scoreFont.loadFromFile("assets/font.ttf");
+
+		leftPaddleScore.setFont(scoreFont);
+		leftPaddleScore.setCharacterSize(50);
+		leftPaddleScore.setString("0");
+		leftPaddleScore.setPosition(128.0f, 0.0f);
+
+		rightPaddleScore.setFont(scoreFont);
+		rightPaddleScore.setCharacterSize(50);
+		rightPaddleScore.setString("0");
+		rightPaddleScore.setPosition(window.getSize().x - 128.0f, 0.0f);
 
 		leftPaddle.setSize(paddleSize);
-		leftPaddle.setPosition(16, yCenter);
+		leftPaddle.setOrigin(paddleSize / 2.0f);
+		leftPaddle.setPosition(16.0f, window.getSize().y / 2.0f);
 
 		rightPaddle.setSize(paddleSize);
-		rightPaddle.setPosition(window.getSize().x - rightPaddle.getLocalBounds().width - 16, yCenter);
+		rightPaddle.setOrigin(paddleSize / 2.0f);
+		rightPaddle.setPosition(window.getSize().x - 16.0f, window.getSize().y / 2.0f);
 
-		ball.setRadius(4);
-		ball.setOrigin(ball.getRadius() / 2, ball.getRadius() / 2);
+		ball.setRadius(4.0f);
+		ball.setOrigin(ball.getRadius() / 2.0f, ball.getRadius() / 2.0f);
+		ball.setPosition(ball.getOrigin());
 
-		leftPaddleScore->setPosition(window.getSize().x / 2 - 128, 0);
-		leftPaddleScore->setTextSize(50);
-		leftPaddleScore->setText("0");
-		leftPaddleScore->getRenderer()->setFont("assets/font.ttf");
-		leftPaddleScore->getRenderer()->setTextColor(tgui::Color::White);
-		add(leftPaddleScore);
-
-		rightPaddle.setPosition(static_cast<float>(window.getSize().x) - 16, yCenter);
-		rightPaddleScore->setPosition(window.getSize().x / 2 + 128, 0);
-		rightPaddleScore->setTextSize(50);
-		rightPaddleScore->setText("0");
-		rightPaddleScore->getRenderer()->setFont("assets/font.ttf");
-		rightPaddleScore->getRenderer()->setTextColor(tgui::Color::White);
-		add(rightPaddleScore);
-
-		separator.setSize(sf::Vector2f(1, window.getSize().y));
-		separator.setPosition(window.getSize().x / 2, 0);
+		separator.setSize(sf::Vector2f(1.0f, static_cast<float>(window.getSize().y)));
+		separator.setPosition(window.getSize().x / 2.0f, 0);
 	}
 
 	void TwoPlayerState::draw(sf::RenderTarget& target)
 	{
-		tgui::Gui::draw();
-
+		target.draw(leftPaddleScore);
+		target.draw(rightPaddleScore);
 		target.draw(leftPaddle);
 		target.draw(rightPaddle);
 		target.draw(ball);
 		target.draw(separator);
 	}
 
-	void TwoPlayerState::handleEvent(const sf::Event& event)
-	{
-		tgui::Gui::handleEvent(event);
-	}
-
 	void TwoPlayerState::update(const float deltaTime)
 	{
 		updatePaddles(deltaTime);
-		updateBall(deltaTime);
+		ball.update(deltaTime, leftPaddle, leftPaddleScore, rightPaddle, rightPaddleScore);
 	}
 
 	void TwoPlayerState::updatePaddles(const float deltaTime)
 	{
 		const auto& window = Game::getInstance().getWindow();
 
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) && leftPaddle.getPosition().y > 0.0f)
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) && leftPaddle.getPosition().y - leftPaddle.getLocalBounds().height / 2 > 0.0f)
 			leftPaddle.move(0, -paddleSpeed * deltaTime);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S) && leftPaddle.getPosition().y < window.getSize().y - leftPaddle.getLocalBounds().height)
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S) && leftPaddle.getPosition().y + leftPaddle.getLocalBounds().height / 2 < window.getSize().y)
 			leftPaddle.move(0, paddleSpeed * deltaTime);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) && rightPaddle.getPosition().y > 0.0f)
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up) && rightPaddle.getPosition().y - rightPaddle.getLocalBounds().height / 2 > 0.0f)
 			rightPaddle.move(0, -paddleSpeed * deltaTime);
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) && rightPaddle.getPosition().y < window.getSize().y - rightPaddle.getLocalBounds().height)
-			rightPaddle.move(0, paddleSpeed * deltaTime);		
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down) && rightPaddle.getPosition().y + rightPaddle.getLocalBounds().height / 2 < window.getSize().y)
+			rightPaddle.move(0, paddleSpeed * deltaTime);
 	}
-
-	void TwoPlayerState::updateBall(const float deltaTime)
-	{
-		const auto& window = Game::getInstance().getWindow();
-
-		//Upper Window Collision
-		if (ball.getPosition().y - ball.getRadius() < 0.0f)
-		{
-			ballAngle = (360 - ballAngle) % 360;
-			ball.setPosition(ball.getPosition().x, ball.getRadius() + 0.1f);
-			Game::getInstance().getSoundManager().getBeepSound().play();
-		}
-
-		//Lower Window Collision
-		if (ball.getPosition().y + ball.getRadius() > window.getSize().y)
-		{
-			ballAngle = (360 - ballAngle) % 360;
-			ball.setPosition(ball.getPosition().x, window.getSize().y - ball.getRadius() - 0.1f);
-			Game::getInstance().getSoundManager().getBeepSound().play();
-		}
-
-		const auto rightPaddleLeft = rightPaddle.getGlobalBounds().left;
-		const auto rightPaddleTop = rightPaddle.getGlobalBounds().top;
-
-		//Left Window Collision
-		if (ball.getPosition().x - ball.getRadius() < 0.0f)
-		{
-			rightPaddleScore->setText(std::to_string(std::stoi(rightPaddleScore->getText().toAnsiString()) + 1));
-			ball.setPosition(rightPaddleLeft - 10, rightPaddleTop + rightPaddle.getLocalBounds().height / 2);
-			ballAngle = 180;
-			Game::getInstance().getSoundManager().getPeepSound().play();
-		}
-
-		const auto leftPaddleRight = leftPaddle.getGlobalBounds().left + leftPaddle.getGlobalBounds().width;
-		const auto leftPaddleTop = leftPaddle.getGlobalBounds().top;
-
-		//Right Window Collision
-		if (ball.getPosition().x + ball.getRadius() > window.getSize().x)
-		{
-			leftPaddleScore->setText(std::to_string(std::stoi(leftPaddleScore->getText().toAnsiString()) + 1));
-			ball.setPosition(leftPaddleRight + 10, leftPaddleTop + leftPaddle.getLocalBounds().height / 2);
-			ballAngle = 0;
-			Game::getInstance().getSoundManager().getPeepSound().play();
-		}
-
-		//Paddle Collision
-		if (ball.getPosition().x + ball.getRadius() < leftPaddle.getPosition().x)
-		{
-			ballAngle = (180 - ballAngle) % 360;
-
-			if (ballAngle > 0 && ballAngle < 90)
-			{
-				ballAngle -= Random::get(45, ballAngle);
-			}
-			else if (ballAngle > 90 && ballAngle < 180)
-			{
-				ballAngle += Random::get(45, 180 - ballAngle);
-			}
-			else if (ballAngle > 180 && ballAngle < 270)
-			{
-				ballAngle -= Random::get(45, ballAngle - 180);
-			}
-			else if (ballAngle > 270 && ballAngle > 360)
-			{
-				ballAngle += Random::get(45, 360 - ballAngle);
-			}
-			else if (ballAngle == 0 || ballAngle == 180)
-			{
-				auto randAngle = Random::get(0, 180);
-				ballAngle = Random::get<bool>() ? randAngle : -randAngle;
-			}
-
-			ballAngle %= 360;
-			ballIsColliding = true;
-			Game::getInstance().getSoundManager().getPlopSound().play();
-		}
-
-		ball.move(ballSpeed * std::cos(ballAngle * 0.0174532925f) * deltaTime, -ballSpeed * std::sin(ballAngle * 0.0174532925f) * deltaTime);
-	}
-
-	//void TwoPlayerState::checkIfGameOver()
-	//{
-
-	//	/*auto leftPaddleScoreboard = std::stoi(leftPaddleScore->getText().toAnsiString());
-	//	auto rightPaddleScoreboard = std::stoi(rightPaddleScore->getText().toAnsiString());
-
-	//	if (leftPaddleScoreboard != leftPaddle.getPoints())
-	//	{
-	//		leftPaddleScore->setText(std::to_string(leftPaddle.getPoints()));
-	//	}
-	//	else if (rightPaddleScoreboard != rightPaddle.getPoints())
-	//	{
-	//		rightPaddleScore->setText(std::to_string(rightPaddle.getPoints()));
-	//	}
-
-	//	if (leftPaddleScoreboard == 11)
-	//	{
-	//		noteLabel->setText("			Player one has won the game!\n		Press space for the Main Menu");
-	//		noteLabel->setTextSize(20);
-	//		noteLabel->setVisible(true);
-	//		over = true;
-	//	}
-	//	else if (rightPaddleScoreboard == 11)
-	//	{
-	//		noteLabel->setText("			Player two has won the game!\n		Press space for the Main Menu");
-	//		noteLabel->setTextSize(20);
-	//		noteLabel->setVisible(true);
-	//		over = true;
-	//	}*/
-	//}
 }
