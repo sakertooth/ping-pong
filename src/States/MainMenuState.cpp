e3764a8cb7d5af8bba1431113da5babcadae8251
sakertooth <sakertooth@gmail.com>
2021-08-12 14:57:53 -0400

    create main menu state

--- src/States/MainMenuState.cpp
+++ src/States/MainMenuState.cpp
@@ -1,9 +1,44 @@
 #include "MainMenuState.hpp"
+#include "../Game.hpp"
+#include <iostream>
+#include <cmath>
+
+void MainMenuState::init() {
+    const auto& window = Game::getInstance().getWindow();
+    const auto xPos = window.getSize().x / 2.0f;
+    const auto yPos = window.getSize().y / 2.0f;
+
+    title.setFont(Game::getInstance().getFont());
+    title.setCharacterSize(45);
+    title.setString("Ping Pong");
+    title.setOrigin(std::round(title.getLocalBounds().width / 2.0f), std::round(title.getLocalBounds().height / 2.0f));
+    title.setPosition(xPos, yPos - 150.0f);
+
+    onePlayerButton.init();
+    onePlayerButton.setText("One Player");
+    onePlayerButton.onClick([]() { std::cout << "clicked\n"; });
+    onePlayerButton.setPosition(xPos, yPos - 50.0f);
+
+    twoPlayerButton.init();
+    twoPlayerButton.setText("Two Player");
+    twoPlayerButton.onClick([]() { std::cout << "clicked\n"; });
+    twoPlayerButton.setPosition(xPos, yPos);
+
+    exitButton.init();
+    exitButton.setText("Exit");
+    exitButton.onClick([]() { std::exit(0); });
+    exitButton.setPosition(xPos, yPos + 50.0f);
+}
 
 void MainMenuState::update(const sf::Time& deltaTime) {
+    onePlayerButton.update(deltaTime);
     twoPlayerButton.update(deltaTime);
+    exitButton.update(deltaTime);
 }
 
 void MainMenuState::draw(sf::RenderTarget& target, sf::RenderStates states) const {
-    twoPlayerButton.draw(target, states);
+    target.draw(onePlayerButton, states);
+    target.draw(twoPlayerButton, states);
+    target.draw(exitButton, states);
+    target.draw(title, states);
 }
--- src/States/MainMenuState.hpp
+++ src/States/MainMenuState.hpp
@@ -5,8 +5,13 @@
 class MainMenuState : public State {
 public:
     virtual void update(const sf::Time& deltaTime) override;
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    virtual void init() override;
 
 private:
+    Button onePlayerButton;
     Button twoPlayerButton;
+    Button exitButton;
+    sf::Text title;
+
+   virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 };
